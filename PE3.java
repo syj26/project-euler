77f1cbe26ab5025b1556cc6590a485329b3b987b
Sophia Jin <sjin60@stuy.edu>
2024-09-19 17:36:26 -0400

    finished up the code, need to use longs not ints

--- PE3.java
+++ PE3.java
@@ -16,8 +16,21 @@ public class PE3 {
     return i-1;
   }
 
+  public static void largestPrimeFactor(long n) {
+    long ans=1; boolean composite=false; long productTracker=1;
+    for (int i=0; nthPrime(i)<=Math.sqrt(n) && (productTracker != n); i++) {
+      if (n%nthPrime(i)==0) {
+        ans=nthPrime(i);
+        composite=true;
+        productTracker*=nthPrime(i);
+        System.out.println(nthPrime(i));
+      }
+    }
+    if (composite==false) {System.out.println("lpf is "+n);}
+    else {System.out.println("lpf is "+ans);}
+  }
   public static void main(String[] args) {
-    System.out.println(isPrime(2));
+    /* System.out.println(isPrime(2));
     System.out.println(isPrime(3));
     System.out.println(isPrime(4));
     System.out.println(isPrime(5));
@@ -26,6 +39,7 @@ public class PE3 {
     System.out.println(nthPrime(1));
     System.out.println(nthPrime(2));
     System.out.println(nthPrime(3));
-    System.out.println(nthPrime(4));
+    System.out.println(nthPrime(4)); */
+    largestPrimeFactor(600851475143L);
   }
 }
