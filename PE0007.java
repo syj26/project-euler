eb5e9de5038efa1305a776ea023be9a16d0a748a
Sophia Jin <sjin60@stuy.edu>
2024-09-19 20:34:55 -0400

    rename

--- PE0007.java
+++ PE0007.java
@@ -1,4 +1,4 @@
-public class PE7 {
+public class PE0007 {
     public static void main(String[] args) {
         System.out.println(Prime.nthPrime(5)); //note n index starts from 0
         System.out.println(Prime.nthPrime(10000));
