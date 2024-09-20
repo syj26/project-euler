c2ca821266ed2be9b46f9e0cda4ef3d826e24e9a
Sophia Jin <sjin60@stuy.edu>
2024-09-19 20:27:06 -0400

    rename

---
+++ PE0006.java
@@ -0,0 +1,10 @@
+public class PE6 {
+    public static int sumSquareDifference(int n) {
+        return n*n*(n+1)*(n+1)/4-n*(n+1)*(2*n+1)/6;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumSquareDifference(10));
+        System.out.println(sumSquareDifference(100));
+    }
+}
