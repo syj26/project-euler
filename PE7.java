93830506f03f7a96748399f77b5cf7e57f1db927
syj26 <sjin60@stuy.edu>
2024-09-26 20:08:59 -0400

    Delete PE7.java

--- PE7.java
+++
@@ -1,6 +0,0 @@
-public class PE7 {
-    public static void main(String[] args) {
-        System.out.println(Prime.nthPrime(5)); //note n index starts from 0
-        System.out.println(Prime.nthPrime(10000));
-    }
-}
